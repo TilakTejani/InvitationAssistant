c8524080846411616059b48510f3e141cf04039c
TilakTejani <tilak.tejani@gmail.com>
2024-03-20 20:49:31 +0530

    Downloading edited pdfs... done

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, Response,redirect,request, url_for
+from flask import Flask, render_template, Response,redirect,request, url_for, send_file
 import fitz
 import os
 import base64
@@ -75,46 +75,47 @@ def csvUpload():
 
 
 def editPdfs(data):
-    for i, row in enumerate(data):
-        pdf = fitz.open(pdffilepath)
-        name = row['Name']
-        for (id,textComp) in textComps.items():
-            print(f"textComp: {textComp}")
-            # print(f"pdf: {pdf}")
-            page = pdf.load_page(textComp["page"]-1)
-            print("Page size of fitz pdf:", page.rect.width, page.rect.height)
-            # appearent x, y -> 500, 800
-            # actually x, y -> 446, 697
+    if textComps:
+        for i, row in enumerate(data):
+            pdf = fitz.open(pdffilepath)
+            name = row['Name']
+            for (id,textComp) in textComps.items():
+                print(f"textComp: {textComp}")
+                # print(f"pdf: {pdf}")
+                page = pdf.load_page(textComp["page"]-1)
+                print("Page size of fitz pdf:", page.rect.width, page.rect.height)
+                # appearent x, y -> 500, 800
+                # actually x, y -> 446, 697
 
-            # (x1, y1) and (x2, y2) to (x1', y1') and (x2', y2')
-            # x' = x1' + (x - x1)(x2' - x1')/(x2 - x1)
-            # y' = y1' + (y - y1)(y2' - y1')/(y2 - y1)
-            
-            # y1 => 0 -> -13     y2 => 802 -> 830
-            # x1 => 0 -> 0       x2 => 502 -> 520
+                # (x1, y1) and (x2, y2) to (x1', y1') and (x2', y2')
+                # x' = x1' + (x - x1)(x2' - x1')/(x2 - x1)
+                # y' = y1' + (y - y1)(y2' - y1')/(y2 - y1)
+                
+                # y1 => 0 -> -13     y2 => 802 -> 830
+                # x1 => 0 -> 0       x2 => 502 -> 520
 
-            x1, y1 = 0, 0
-            x1_new, y1_new = 0, -13
+                x1, y1 = 0, 0
+                x1_new, y1_new = 0, -13
 
-            x2, y2 = 502, 802 
-            x2_new, y2_new = 520, 830
+                x2, y2 = 502, 802 
+                x2_new, y2_new = 520, 830
 
-            x = int(textComp["x"].replace("px","")) 
-            y = int(textComp["y"].replace("px","")) 
+                x = int(textComp["x"].replace("px","")) 
+                y = int(textComp["y"].replace("px","")) 
 
-            # print("First x and y", x, y)
-            
-            x = x1_new + (x - x1)*(x2_new - x1_new)/(x2 - x1)
-            y = y1_new + (y - y1)*(y2_new - y1_new)/(y2 - y1)
-            
-            # print("After x and y", x, y)
-            textHtml = f"<p style='color: {textComp['color']};font-size:{textComp['fontSize'] + 2}px;'>{name}</p>"
-            page.insert_htmlbox(fitz.Rect(x,y,x+1000,y+1000),textHtml)
-            print(f"text added : {x} {y}")
-            os.makedirs(os.path.join(f'{bride_groom_Name}', 'createdPdfs'), exist_ok=True)
-            pdfPath  = os.path.join(f'{bride_groom_Name}/createdPdfs',f'{name}.pdf')
-            data[i]['filePath'] = pdfPath
-            pdf.save(f"{bride_groom_Name}/createdPdfs/{name}.pdf")
+                # print("First x and y", x, y)
+                
+                x = x1_new + (x - x1)*(x2_new - x1_new)/(x2 - x1)
+                y = y1_new + (y - y1)*(y2_new - y1_new)/(y2 - y1)
+                
+                # print("After x and y", x, y)
+                textHtml = f"<p style='color: {textComp['color']};font-size:{textComp['fontSize'] + 2}px;'>{name}</p>"
+                page.insert_htmlbox(fitz.Rect(x,y,x+1000,y+1000),textHtml)
+                print(f"text added : {x} {y}")
+                os.makedirs(os.path.join(f'{bride_groom_Name}', 'createdPdfs'), exist_ok=True)
+                pdfPath  = os.path.join(f'{bride_groom_Name}/createdPdfs',f'{name}.pdf')
+                data[i]['filePath'] = pdfPath
+                pdf.save(f"{bride_groom_Name}/createdPdfs/{name}.pdf")
     return data
 
 @app.route('/pdfSubmit', methods=['GET', 'POST'])
@@ -194,6 +195,11 @@ def textSubmit():
         textComps = json.loads(textComps)
         generatePdf()
     return redirect("/")
+
+@app.route('/<path:filepath>', methods = ['GET'])
+def download(filepath):
+    print(filepath)
+    return send_file(filepath, as_attachment=True)
 
 if __name__ == '__main__':
     os.makedirs('local', exist_ok=True)
